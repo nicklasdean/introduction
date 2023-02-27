571dca37efa53684c96f3175027a265d034ddc26
dean <nicklasfrederiksen@gmail.com>
2023-02-27 17:57:18 +0100

    First

---
+++ app/src/main/java/com/example/introduction/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.introduction;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity", "Application now on pause");
+    }
+}
